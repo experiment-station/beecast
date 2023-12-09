64633b105b75b0a5c570d2d543d69fdda3d33cb9
Altay <altay@zebrastik.com>
2023-12-09 23:06:24 +0200

    feat: install radix-themes (#4)
    feat: install radix-themes
    
    feat: update app to use radix-themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,96 +1,17 @@
-import Image from 'next/image';
+import { Button, Container, Flex, Heading, Text } from '@radix-ui/themes';
 
-import styles from './page.module.css';
-
-export default function Home() {
+export default function Page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            By{' '}
-            <Image
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              height={24}
-              priority
-              src="/vercel.svg"
-              width={100}
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          alt="Next.js Logo"
-          className={styles.logo}
-          height={37}
-          priority
-          src="/next.svg"
-          width={180}
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          className={styles.card}
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          className={styles.card}
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          className={styles.card}
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          className={styles.card}
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Container
+      size={{
+        initial: '1',
+      }}
+    >
+      <Flex direction="column" gap="2">
+        <Heading>Hello World</Heading>
+        <Text>Hello from Radix Themes 🤓</Text>
+        <Button>Click me</Button>
+      </Flex>
+    </Container>
   );
 }
