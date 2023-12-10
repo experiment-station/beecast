e9430f45760612b3c812af40a0c94e675ce64191
Okan Işıldar <okanisildar@gmail.com>
2023-12-10 21:57:05 +0300

    feat: supa-4 nextjs supabase integration (#10)
    * feat: install supabase, add generate types script, add generated db types
    
    * feat: implement supabase auth boilerplate and supabase sdks for runtimes. lint database.ts
    
    * chore: move supabase folder into lib/services
    
    * feat: spotify oauth
    
    * chore: use env.mjs for env variables
    
    * chore: remove unnecessary package

---
+++ lib/services/supabase/auth.ts
@@ -0,0 +1,63 @@
+import type { Database } from '@/types/database';
+import type { CookieOptions } from '@supabase/ssr';
+import type { NextRequest } from 'next/server';
+
+import { env } from '@/env.mjs';
+import { createServerClient } from '@supabase/ssr';
+import { NextResponse } from 'next/server';
+
+export const getSupabaseAuthSession = async (request: NextRequest) => {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient<Database>(
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        remove(name, options) {
+          request.cookies.delete({
+            name,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.delete({
+            name,
+            ...options,
+          });
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+      },
+    },
+  );
+
+  const authSession = await supabase.auth.getSession();
+
+  return { authSession, response };
+};
---
+++ lib/services/supabase/service.ts
@@ -0,0 +1,7 @@
+import type { Database } from '@/types/database';
+
+import { env } from '@/env.mjs';
+import { createClient } from '@supabase/supabase-js';
+
+export const createSupabaseServiceClient = () =>
+  createClient<Database>(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY);
