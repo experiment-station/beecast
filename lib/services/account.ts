c247c07b020268ab0d2ddc3079810246d3103f74
Altay <altay@zebrastik.com>
2023-12-11 10:37:01 +0300

    feat: init account service

---
+++ lib/services/account.ts
@@ -0,0 +1,53 @@
+import type { Tables } from '@/types/supabase/database';
+
+import { createSupabaseServiceClient } from './supabase/service';
+
+export const fetchAccountAICredits = async (
+  accountId: Tables<'account'>['id'],
+) => {
+  const supabase = createSupabaseServiceClient();
+
+  const accountQuery = await supabase
+    .from('account')
+    .select('ai_credit')
+    .eq('id', accountId)
+    .single();
+
+  if (accountQuery.error) {
+    throw new Error(accountQuery.error.message);
+  }
+
+  return accountQuery.data.ai_credit || 0;
+};
+
+export const validateAccountAICredits = async (
+  accountId: Tables<'account'>['id'],
+) => {
+  const aiCredits = await fetchAccountAICredits(accountId);
+
+  if (aiCredits <= 0) {
+    throw new Error('User does not have any AI credits');
+  }
+
+  return aiCredits;
+};
+
+export const updateAccountAICredits = async (
+  accountId: Tables<'account'>['id'],
+  amount: number,
+) => {
+  const supabase = createSupabaseServiceClient();
+
+  const accountQuery = await supabase
+    .from('account')
+    .update({ ai_credit: amount })
+    .eq('id', accountId)
+    .select('ai_credit')
+    .single();
+
+  if (accountQuery.error) {
+    throw new Error(accountQuery.error.message);
+  }
+
+  return accountQuery.data.ai_credit || 0;
+};
