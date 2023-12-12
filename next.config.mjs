2902e282eb1d011e726f3639ff9f341ae1477f21
Altay <altay@zebrastik.com>
2023-12-12 17:45:17 +0200

    feat: add redirections to privacy policy and terms of service (#18)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 import './env.mjs';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => [
+    {
+      destination:
+        'https://experiment-station.notion.site/Privacy-Policy-for-Beecast-c69c47d343694b8884eede01f8a61341?pvs=4',
+      permanent: false,
+      source: '/privacy-policy',
+    },
+    {
+      destination:
+        'https://experiment-station.notion.site/Terms-of-Service-for-Beecast-85524ec372614383b4d86156e47e5df7?pvs=4',
+      permanent: false,
+      source: '/terms-of-service',
+    },
+  ],
+};
 
 export default nextConfig;
