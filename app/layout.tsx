298aeeff275daf122fe89c868e1c62447c405f40
Altay <altay@zebrastik.com>
2023-12-12 17:49:40 +0200

    refactor: isolate home and auth layouts (#22)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,19 @@
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 
-import { AppLayout } from '@/components/layout/app-layout';
 import { ThemeProvider } from '@/components/theme-provider';
 import '@/components/theme-provider/styles.css';
 
 export const metadata: Metadata = {
-  description: 'Welcome, welcome, welcome!',
-  title: 'beecast',
+  description: 'Your hard-working AI podcast companion.',
+  title: 'Beecast',
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider>
-          <AppLayout>{children}</AppLayout>
-        </ThemeProvider>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
