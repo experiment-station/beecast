4c40ed785130484e79ac98174065f404f9e45bc0
Altay <altay@zebrastik.com>
2023-12-13 19:49:54 +0200

    feat: add extendable errors with es6-error (#34)
    Introduces our identifiable errors within so we can track them down gracefully.

---
+++ lib/errors.ts
@@ -0,0 +1,61 @@
+import ExtendableError from 'es6-error';
+import { NextResponse } from 'next/server';
+
+type ServerErrorParams = {
+  error_message: string;
+  error_type: string;
+  status_code: number;
+  underlyingError?: unknown;
+};
+
+export class ServerError extends ExtendableError {
+  params: ServerErrorParams;
+  trace_id?: string;
+
+  constructor(params: ServerErrorParams) {
+    super(params.error_type);
+    this.params = params;
+  }
+
+  toJSON() {
+    return {
+      error_id: this.trace_id,
+      error_message: this.params.error_message,
+      error_type: this.params.error_type,
+      status_code: this.params.status_code,
+    };
+  }
+
+  toNextResponse() {
+    return NextResponse.json(this.toJSON(), {
+      status: this.params.status_code,
+    });
+  }
+}
+
+type ExtendedServerErrorParams = {
+  error?: unknown;
+  message?: string;
+  type?: string;
+};
+
+export class InternalServerError extends ServerError {
+  constructor(params: ExtendedServerErrorParams = {}) {
+    super({
+      error_message: params.message ?? 'Internal server error',
+      error_type: params.type ?? 'INTERNAL_SERVER_ERROR',
+      status_code: 500,
+      underlyingError: params.error,
+    });
+  }
+}
+
+export class BadRequestError extends ServerError {
+  constructor(params: ExtendedServerErrorParams = {}) {
+    super({
+      error_message: params.message ?? 'Bad request',
+      error_type: params.type ?? 'INVALID_REQUEST',
+      status_code: 400,
+    });
+  }
+}
