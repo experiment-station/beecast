5a115cc7f26e8033b70aadb6090604fb4efbfa4f
Okan Işıldar <okanisildar@gmail.com>
2023-12-13 20:20:44 +0300

    feat: save show service and some chore for renaming a type (#30)
    * feat: search shows on podcast index
    
    * chore: create podcastIndexClient, move packages under libs
    
    * chore: add eslint disabling line for node:crypto
    
    * chore: import env variables from envmjs
    
    * feat: save show service and some chore for renaming a type
    
    * chore: use supabase service client instead of browser and change env variables
    
    * chore: change spotify_id type

---
+++ lib/services/show.ts
@@ -0,0 +1,31 @@
+import type { Tables } from '@/types/supabase/database-generated';
+
+import type { PodcastIndexShowType } from './podcast-index/search-show';
+
+import { createSupabaseServiceClient } from './supabase/service';
+
+export const saveShow = async (
+  show: PodcastIndexShowType,
+  spotifyId: Tables<'show'>['spotify_id'],
+) => {
+  const supabase = createSupabaseServiceClient();
+  const { data, error } = await supabase
+    .from('show')
+    .insert({
+      description: show.description,
+      images: [show.image],
+      language: show.language,
+      podcast_index_guid: show.podcastGuid,
+      publisher: show.author,
+      spotify_id: spotifyId,
+      title: show.title,
+      total_episode: show.episodeCount,
+    })
+    .select();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};
