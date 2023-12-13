99dcae7de79eb6686108a8da82a373d79845f5db
Altay <altay@zebrastik.com>
2023-12-13 11:52:18 +0300

    chore: refactor auth flow

--- lib/services/spotify/save-user-info.ts
+++ lib/services/spotify/save-user-info.ts
@@ -1,7 +1,7 @@
 import type { Session, User } from '@supabase/supabase-js';
 
+import { BadRequestError, InternalServerError } from '@/lib/errors';
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
 
 import { createSupabaseServerClient } from '../supabase/server';
 
@@ -20,36 +20,36 @@ export const saveUserInfo = async ({
 }) => {
   const providerToken = session.provider_token;
   const userMetadata: UserMetadata = user.user_metadata;
-  if (
-    providerToken &&
-    userMetadata.avatar_url &&
-    userMetadata.full_name &&
-    userMetadata.provider_id
-  ) {
-    const cookieStore = cookies();
-    const supabase = createSupabaseServerClient(cookieStore);
-    const { error } = await supabase
-      .from('account')
-      .upsert(
-        {
-          avatar_url: userMetadata.avatar_url,
-          display_name: userMetadata.full_name,
-          provider_refresh_token: session.provider_refresh_token,
-          provider_token: providerToken,
-          spotify_id: userMetadata.provider_id,
-          user_id: user.id,
-        },
-        { onConflict: 'spotify_id' },
-      )
-      .select();
 
-    if (error) {
-      return NextResponse.json(
-        { message: 'Spotify into could not save into account table' },
-        {
-          status: 500,
-        },
-      );
-    }
+  if (
+    !providerToken ||
+    !userMetadata.avatar_url ||
+    !userMetadata.full_name ||
+    !userMetadata.provider_id
+  ) {
+    throw new BadRequestError({
+      message: 'Missing user info',
+    });
+  }
+
+  const supabase = createSupabaseServerClient(cookies());
+
+  const { error } = await supabase
+    .from('account')
+    .upsert(
+      {
+        avatar_url: userMetadata.avatar_url,
+        display_name: userMetadata.full_name,
+        provider_refresh_token: session.provider_refresh_token,
+        provider_token: providerToken,
+        spotify_id: userMetadata.provider_id,
+        user_id: user.id,
+      },
+      { onConflict: 'spotify_id' },
+    )
+    .select();
+
+  if (error) {
+    throw new InternalServerError({ error });
   }
 };
