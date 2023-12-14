f29bd8ef64b35944eddccd43b24c3da9674f15fd
Altay <altay@zebrastik.com>
2023-12-14 18:14:20 +0200

    feat: init `shows` page [SUPA-10] (#45)
    * feat: add basic layout for dashboard
    
    * feat: configure storybook theming
    
    * chore: add `no-open` to storybook dev args
    
    * feat: add `ui/hover` component
    
    * feat: add `show-card` component
    
    * refactor: use css modules
    
    * feat: init dashboard/shows page
    
    * feat: update layout
    
    * feat: show all shows

---
+++ app/(dashboard)/shows/page.tsx
@@ -0,0 +1,65 @@
+import { ShowCard } from '@/components/show-card';
+import { DatabaseError } from '@/lib/errors';
+import { getAccountId } from '@/lib/services/account';
+import { createSupabaseServerClient } from '@/lib/services/supabase/server';
+import { Flex, Grid, Heading } from '@radix-ui/themes';
+import { cookies } from 'next/headers';
+
+const fetchAllShows = async () => {
+  const supabase = createSupabaseServerClient(cookies());
+  const response = await supabase
+    .from('show')
+    .select('id, title, description, images');
+
+  return response;
+};
+
+const _fetchMyShows = async () => {
+  const supabase = createSupabaseServerClient(cookies());
+  const accountId = await getAccountId();
+  const response = await supabase
+    .from('account_show_relation')
+    .select('id, show (title, description, images)')
+    .eq('account', accountId);
+
+  return response;
+};
+
+export default async function Page() {
+  const { data, error } = await fetchAllShows();
+
+  if (error) {
+    throw new DatabaseError(error);
+  }
+
+  return (
+    <Flex direction="column" gap="4">
+      <Heading as="h2" size="6">
+        Your shows
+      </Heading>
+
+      <Grid
+        columns={{
+          initial: '2',
+          md: '5',
+          sm: '4',
+          xs: '3',
+        }}
+        gap={{
+          initial: '4',
+          md: '5',
+        }}
+      >
+        {data.map((show) => (
+          <ShowCard.Link
+            description={show.description}
+            href={`/shows/${show.id}`}
+            images={show.images}
+            key={show.id}
+            title={show.title}
+          />
+        ))}
+      </Grid>
+    </Flex>
+  );
+}
