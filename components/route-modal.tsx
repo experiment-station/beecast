95c62207d5bff52e07a2ea5b196df825ee556360
Okan Işıldar <okanisildar@gmail.com>
2023-12-14 23:51:06 +0300

    feat: implement onboarding modal

--- components/route-modal.tsx
+++ components/route-modal.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import type { Responsive } from '@radix-ui/themes';
 import type { PropsWithChildren } from 'react';
 
 import { Dialog } from '@radix-ui/themes';
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 
-export function RouteModal({ children }: PropsWithChildren) {
+export function RouteModal({
+  children,
+  size = '3',
+}: PropsWithChildren<{
+  size?: Responsive<'1' | '2' | '3' | '4'>;
+}>) {
   const router = useRouter();
 
   const onDismiss = useCallback(() => {
@@ -18,6 +24,7 @@ export function RouteModal({ children }: PropsWithChildren) {
       <Dialog.Content
         onEscapeKeyDown={onDismiss}
         onPointerDownOutside={onDismiss}
+        size={size}
       >
         {children}
       </Dialog.Content>
