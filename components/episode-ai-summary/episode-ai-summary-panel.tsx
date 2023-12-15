74a8635061e347964391a35a0b64584d20483511
Altay <altay@zebrastik.com>
2023-12-15 20:10:25 +0300

    feat: add `ai` badge to `episode-ai-summary-panel`'s `title`

--- components/episode-ai-summary/episode-ai-summary-panel.tsx
+++ components/episode-ai-summary/episode-ai-summary-panel.tsx
@@ -1,5 +1,8 @@
 import type { PropsWithChildren } from 'react';
 
+import { Badge, Flex } from '@radix-ui/themes';
+import { PiRobotBold } from 'react-icons/pi';
+
 import { CollapsiblePanel } from '../ui/collapsible-panel';
 import { Panel } from '../ui/panel';
 import { PreWrap } from '../ui/pre-wrap';
@@ -8,16 +11,35 @@ type Props = PropsWithChildren<{
   variant?: 'collapsible' | 'default';
 }>;
 
+function Title() {
+  return (
+    <Flex align="center" gap="2">
+      <Badge
+        color="mint"
+        size="1"
+        style={{
+          marginBottom: '-1px',
+        }}
+      >
+        <PiRobotBold />
+        AI
+      </Badge>
+
+      <span>Episode summary</span>
+    </Flex>
+  );
+}
+
 export function EpisodeAISummaryPanel({
   children,
   variant = 'default',
 }: Props) {
   return variant === 'collapsible' ? (
-    <CollapsiblePanel title="Episode summary">
+    <CollapsiblePanel title={<Title />}>
       <PreWrap>{children}</PreWrap>
     </CollapsiblePanel>
   ) : (
-    <Panel title="Episode summary">
+    <Panel title={<Title />}>
       <PreWrap>{children}</PreWrap>
     </Panel>
   );
