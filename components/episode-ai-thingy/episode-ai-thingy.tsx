02e7dbbc29c80f580d9b6e692682df0871554d6c
Altay <altay@zebrastik.com>
2023-12-15 13:26:59 +0300

    refactor: clean-up ai-thingy

--- components/episode-ai-thingy/episode-ai-thingy.tsx
+++ components/episode-ai-thingy/episode-ai-thingy.tsx
@@ -3,12 +3,14 @@ import type { Tables } from '@/types/supabase/database';
 import { DatabaseError } from '@/lib/errors';
 import { fetchAccountAICredits } from '@/lib/services/account';
 import { createSupabaseServerClient } from '@/lib/services/supabase/server';
-import { Avatar, Box, Flex, Text } from '@radix-ui/themes';
+import { Avatar, Box, Button, Flex, Text } from '@radix-ui/themes';
 import { cookies } from 'next/headers';
-import { CgProfile } from 'react-icons/cg';
+import Link from 'next/link';
+import { CgDollar, CgProfile } from 'react-icons/cg';
 
 import { CollapsiblePanel } from '../ui/collapsible-panel';
 import { EpisodeAIThingyGenerator } from './episode-ai-thingy-generator';
+import { EpisodeAIThingyPlaceholder } from './episode-ai-thingy-placeholder';
 
 type Props = {
   id: Tables<'episode'>['id'];
@@ -16,9 +18,7 @@ type Props = {
 
 export async function EpisodeAIThingy(props: Props) {
   const supabase = createSupabaseServerClient(cookies());
-
   const credits = await fetchAccountAICredits();
-
   const { data, error } = await supabase
     .from('episode_content')
     .select('*, user:account(id, display_name, avatar_url)')
@@ -29,7 +29,19 @@ export async function EpisodeAIThingy(props: Props) {
   }
 
   if (data.length === 0) {
-    return <EpisodeAIThingyGenerator credits={credits} id={props.id} />;
+    if (credits < 1) {
+      return (
+        <EpisodeAIThingyPlaceholder>
+          <Button asChild highContrast>
+            <Link href="/credits">
+              <CgDollar /> Buy credits to summarize this episode
+            </Link>
+          </Button>
+        </EpisodeAIThingyPlaceholder>
+      );
+    }
+
+    return <EpisodeAIThingyGenerator id={props.id} />;
   }
 
   const [episodeContent] = data;
