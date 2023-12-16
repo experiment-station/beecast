483a14b6b344c71b3bfccddf6b357cc2b9141305
Altay <altay@zebrastik.com>
2023-12-16 14:07:42 +0200

    feat: add user info to stripe checkout session (#75)

--- lib/services/stripe/checkout.ts
+++ lib/services/stripe/checkout.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { getAccountId } from '../account';
+import { getUser } from '../supabase/auth';
 import { stripe } from './client';
 
 export const createCheckoutSession = async ({
@@ -9,17 +11,27 @@ export const createCheckoutSession = async ({
   baseUrl: string;
   priceId: string;
 }) => {
-  const session = await stripe.checkout.sessions.create({
+  const user = await getUser();
+  const accountId = await getAccountId();
+
+  const checkoutSession = await stripe.checkout.sessions.create({
     cancel_url: `${baseUrl}?status=cancel`,
+    customer_email: user.email,
     line_items: [
       {
         price: priceId,
         quantity: 1,
       },
     ],
+    metadata: {
+      accountId,
+      userId: user.id,
+    },
     mode: 'payment',
     success_url: `${baseUrl}?status=success`,
   });
 
-  return session;
+  return {
+    url: checkoutSession.url,
+  };
 };
