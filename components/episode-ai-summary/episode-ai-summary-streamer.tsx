a4d711a4e43c94a9ffc60409114ee96d19f853e0
Altay <altay@zebrastik.com>
2023-12-16 22:16:31 +0200

    feat: extract `ai-summary-footer` (#90)
    feat: add episode-ai-summary-footer
    
    refactor: simplify episode-summary query

--- components/episode-ai-summary/episode-ai-summary-streamer.tsx
+++ components/episode-ai-summary/episode-ai-summary-streamer.tsx
@@ -2,8 +2,12 @@
 
 import type { Tables } from '@/types/supabase/database';
 
+import { Flex } from '@radix-ui/themes';
 import { useChat } from 'ai/react';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { EpisodeAISummaryFooter } from './episode-ai-summary-footer';
+import { EpisodeAISummaryPanel } from './episode-ai-summary-panel';
 
 type Props = {
   id: Tables<'episode'>['id'];
@@ -11,9 +15,13 @@ type Props = {
 
 export function EpisodeAISummaryStreamer({ id }: Props) {
   const startedRef = useRef(false);
+  const [finished, setFinished] = useState(false);
 
   const { messages, reload, setMessages } = useChat({
     api: `/api/ai/summarize/${id}`,
+    onFinish: () => {
+      setFinished(true);
+    },
   });
 
   useEffect(() => {
@@ -28,15 +36,19 @@ export function EpisodeAISummaryStreamer({ id }: Props) {
 
   const assistantMessages = messages.filter((m) => m.role === 'assistant');
 
-  if (assistantMessages.length === 0) {
-    return 'Summarizing episode...';
-  }
-
   return (
-    <>
-      {assistantMessages.map((message) => (
-        <React.Fragment key={message.id}>{message.content}</React.Fragment>
-      ))}
-    </>
+    <Flex direction="column" gap="2">
+      <EpisodeAISummaryPanel>
+        {assistantMessages.length === 0
+          ? 'Summarizing episode...'
+          : assistantMessages.map((message) => (
+              <React.Fragment key={message.id}>
+                {message.content}
+              </React.Fragment>
+            ))}
+      </EpisodeAISummaryPanel>
+
+      {finished ? <EpisodeAISummaryFooter id={id} /> : null}
+    </Flex>
   );
 }
