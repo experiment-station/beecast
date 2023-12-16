5ea40b53ea07a2974305b7d0c5f547fac47b16fa
Okan Işıldar <okanisildar@gmail.com>
2023-12-16 17:31:06 +0300

    feat: add your shows to shows page

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,8 +1,10 @@
+import type { Tables } from '@/types/supabase/database';
+
 import { ShowCard } from '@/components/show-card';
 import { DatabaseError } from '@/lib/errors';
 import { getAccountId } from '@/lib/services/account';
 import { createSupabaseServerClient } from '@/lib/services/supabase/server';
-import { Flex, Grid, Heading } from '@radix-ui/themes';
+import { Flex, Grid, Heading, Separator } from '@radix-ui/themes';
 import { cookies } from 'next/headers';
 
 const fetchAllShows = async () => {
@@ -14,28 +16,81 @@ const fetchAllShows = async () => {
   return response;
 };
 
-const _fetchMyShows = async () => {
+const fetchMyShows = async () => {
   const supabase = createSupabaseServerClient(cookies());
   const accountId = await getAccountId();
   const response = await supabase
     .from('account_show_relation')
-    .select('id, show (title, description, images)')
-    .eq('account', accountId);
+    .select('show (id, title, description, images)')
+    .eq('account', accountId)
+    .not('show', 'is', null);
 
   return response;
 };
 
 export default async function Page() {
-  const { data, error } = await fetchAllShows();
+  const { data, error } = await fetchMyShows();
+  const response = await fetchAllShows();
+  let showsData = response.data;
 
   if (error) {
     throw new DatabaseError(error);
   }
 
+  if (response.error) {
+    throw new DatabaseError(response.error);
+  }
+
+  const myShows = data.filter(
+    (i): i is { show: Tables<'show'> } => i.show !== null,
+  );
+
+  if (myShows.length > 0) {
+    showsData =
+      showsData?.filter((i) => !myShows.map((s) => s.show.id).includes(i.id)) ??
+      [];
+  }
+
   return (
     <Flex direction="column" gap="4">
+      {myShows.length > 0 && (
+        <>
+          <Heading as="h2" size="6">
+            Your shows
+          </Heading>
+          <Grid
+            columns={{
+              initial: '2',
+              md: '4',
+              xs: '3',
+            }}
+            gap={{
+              initial: '4',
+              md: '5',
+            }}
+          >
+            {myShows.map(({ show }) => (
+              <ShowCard.Link
+                description={show.description}
+                href={`/shows/${show.id}`}
+                images={show.images}
+                key={show.id}
+                title={show.title}
+              />
+            ))}
+          </Grid>
+          <Separator
+            orientation="horizontal"
+            size="4"
+            style={{
+              color: 'var(--gray-12)',
+              margin: '1em 0',
+            }}
+          />
+        </>
+      )}
       <Heading as="h2" size="6">
-        Your shows
+        If you want to give a shot
       </Heading>
 
       <Grid
@@ -49,7 +104,7 @@ export default async function Page() {
           md: '5',
         }}
       >
-        {data.map((show) => (
+        {showsData?.map((show) => (
           <ShowCard.Link
             description={show.description}
             href={`/shows/${show.id}`}
