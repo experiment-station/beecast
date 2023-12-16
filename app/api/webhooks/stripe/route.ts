717d77ebbda2bfe744dce0a093e54855dc13442c
Altay <altay@zebrastik.com>
2023-12-16 14:14:01 +0200

    feat: add stripe webhook (#76)
    feat: add  to env
    
    feat: add more params to stripe client
    
    feat: add stripe webhook
    
    feat: update parser

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,59 @@
+import type { NextRequest } from 'next/server';
+import type Stripe from 'stripe';
+
+import { env } from '@/env.mjs';
+import { HttpBadRequestError, HttpInternalServerError } from '@/lib/errors';
+import { stripe } from '@/lib/services/stripe/client';
+import { fulfillOrder } from '@/lib/services/stripe/order';
+
+export async function POST(req: NextRequest) {
+  const signature = req.headers.get('stripe-signature');
+
+  if (!signature) {
+    return new HttpBadRequestError({
+      message: 'Missing Stripe signature',
+    }).toNextResponse();
+  }
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      signature,
+      env.STRIPE_WEBHOOK_SECRET,
+    );
+  } catch (error) {
+    return new HttpBadRequestError({
+      message: 'Invalid payload',
+    }).toNextResponse();
+  }
+
+  try {
+    switch (event.type) {
+      case 'checkout.session.completed': {
+        const session = await stripe.checkout.sessions.retrieve(
+          event.data.object.id,
+          {
+            expand: ['line_items'],
+          },
+        );
+
+        if (session.payment_status === 'paid') {
+          await fulfillOrder(session);
+        }
+
+        break;
+      }
+
+      default:
+        break;
+    }
+  } catch (error) {
+    return new HttpInternalServerError({
+      error,
+    }).toNextResponse();
+  }
+
+  return new Response('OK');
+}
--- lib/services/stripe/client.ts
+++ lib/services/stripe/client.ts
@@ -2,6 +2,9 @@ import { env } from '@/env.mjs';
 import { createExternalServiceError } from '@/lib/errors';
 import { Stripe } from 'stripe';
 
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY);
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+  typescript: true,
+});
 
 export const StripeError = createExternalServiceError('Stripe');
