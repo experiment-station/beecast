0bd3ec7b8ad48dce6225d55605a9c282f0cd958f
Altay <altay@zebrastik.com>
2023-12-16 17:29:04 +0300

    chore: add og:url

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -4,11 +4,15 @@ export const SITE_NAME = 'beecast.ai';
 export const SITE_DESCRIPTION = 'Your hard-working AI podcast companion';
 export const SITE_TITLE = 'beecast';
 
+const SITE_URL = process.env.VERCEL_URL || 'http://localhost:3000';
+
 export const getDefaultMetadata = (): Metadata => ({
   description: SITE_DESCRIPTION,
+  metadataBase: new URL(SITE_URL),
   openGraph: {
     description: SITE_DESCRIPTION,
     title: SITE_TITLE,
+    url: SITE_URL,
   },
   title: SITE_TITLE,
   twitter: {
@@ -16,7 +20,4 @@ export const getDefaultMetadata = (): Metadata => ({
     description: SITE_DESCRIPTION,
     title: SITE_TITLE,
   },
-  ...(process.env.NODE_ENV === 'development'
-    ? { metadataBase: new URL('http://localhost:3000') }
-    : {}),
 });
