ddcba33d47998f79791a10462877758b6588b7ec
Altay <altay@zebrastik.com>
2023-12-17 13:03:27 +0300

    feat: increment/decrement `ai_credits` on `ai` services

--- lib/services/ai/create-episode-summary-stream.ts
+++ lib/services/ai/create-episode-summary-stream.ts
@@ -4,6 +4,7 @@ import type { OpenAI } from 'openai';
 import { OpenAIStream } from 'ai';
 import { promptTokensEstimate } from 'openai-chat-tokens';
 
+import { incrementAccountAICredits } from '../account';
 import { saveEpisodeContentSummary } from '../episode-content';
 import { openai } from './openai';
 
@@ -26,32 +27,37 @@ export async function createEpisodeSummaryStream({
   episode: Pick<Tables<'episode'>, 'id' | 'title'>;
   transcription: string;
 }) {
-  const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
-    {
-      content: `
+  try {
+    const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+      {
+        content: `
         Summarize the following transcription of the podcast episode "${episode.title}" by following these guidelines:
           - Skip the ads and intro.
           - Use paragraphs or bullet point lists.
           - Prefer short, clean sentences.
           - Write the summary in the same language as the transcription.
       `,
-      role: 'system',
-    },
-    {
-      content: transcription,
-      role: 'system',
-    },
-  ];
+        role: 'system',
+      },
+      {
+        content: transcription,
+        role: 'system',
+      },
+    ];
 
-  const response = await openai.chat.completions.create({
-    messages,
-    model: getModel(promptTokensEstimate({ messages })),
-    stream: true,
-  });
+    const response = await openai.chat.completions.create({
+      messages,
+      model: getModel(promptTokensEstimate({ messages })),
+      stream: true,
+    });
 
-  return OpenAIStream(response, {
-    onCompletion: async (completion) => {
-      await saveEpisodeContentSummary(episode.id, completion);
-    },
-  });
+    return OpenAIStream(response, {
+      onCompletion: async (completion) => {
+        await saveEpisodeContentSummary(episode.id, completion);
+      },
+    });
+  } catch (error) {
+    await incrementAccountAICredits();
+    throw error;
+  }
 }
