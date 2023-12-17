a7d004db113150c60fd46a1c9b3eef77172c5f5b
Altay <altay@zebrastik.com>
2023-12-17 14:14:25 +0200

    feat: increment/decrement `ai_credits` on `ai` services (#100)

--- lib/services/ai/transcribe-episode.ts
+++ lib/services/ai/transcribe-episode.ts
@@ -6,60 +6,71 @@ import { DatabaseError } from '@/lib/errors';
 import { getFinalRedirectURL } from '@/lib/utils/get-final-redirect-url';
 import { cookies } from 'next/headers';
 
-import { getAccountId } from '../account';
+import {
+  decrementAccountAICredits,
+  getAccountId,
+  incrementAccountAICredits,
+} from '../account';
 import { createSupabaseServerClient } from '../supabase/server';
 import { transcribeAudio } from './deepgram';
 
 export const transcribeEpisode = async (id: Tables<'episode'>['id']) => {
-  const supabase = createSupabaseServerClient(cookies());
+  try {
+    await decrementAccountAICredits();
 
-  const existingEpisodeContentQuery = await supabase
-    .from('episode_content')
-    .select('transcription')
-    .eq('episode', id);
+    const supabase = createSupabaseServerClient(cookies());
 
-  if (existingEpisodeContentQuery.error) {
-    throw new DatabaseError(existingEpisodeContentQuery.error);
+    const existingEpisodeContentQuery = await supabase
+      .from('episode_content')
+      .select('transcription')
+      .eq('episode', id);
+
+    if (existingEpisodeContentQuery.error) {
+      throw new DatabaseError(existingEpisodeContentQuery.error);
+    }
+
+    if (existingEpisodeContentQuery.data[0]?.transcription) {
+      return existingEpisodeContentQuery.data[0].transcription;
+    }
+
+    const episodeQuery = await supabase
+      .from('episode')
+      .select('audio_url, show(language)')
+      .eq('id', id)
+      .single();
+
+    if (episodeQuery.error) {
+      throw new DatabaseError(episodeQuery.error);
+    }
+
+    const url = await getFinalRedirectURL(episodeQuery.data.audio_url);
+    const transcription = await transcribeAudio({
+      language: episodeQuery.data.show?.language ?? undefined,
+      url,
+    });
+
+    const updateEpisodeContentQuery = await supabase
+      .from('episode_content')
+      .upsert(
+        {
+          account: await getAccountId(),
+          episode: id,
+          transcription,
+        },
+        {
+          onConflict: 'episode',
+        },
+      )
+      .select('transcription')
+      .single();
+
+    if (updateEpisodeContentQuery.error) {
+      throw new DatabaseError(updateEpisodeContentQuery.error);
+    }
+
+    return transcription;
+  } catch (error) {
+    await incrementAccountAICredits();
+    throw error;
   }
-
-  if (existingEpisodeContentQuery.data[0]?.transcription) {
-    return existingEpisodeContentQuery.data[0].transcription;
-  }
-
-  const episodeQuery = await supabase
-    .from('episode')
-    .select('audio_url, show(language)')
-    .eq('id', id)
-    .single();
-
-  if (episodeQuery.error) {
-    throw new DatabaseError(episodeQuery.error);
-  }
-
-  const url = await getFinalRedirectURL(episodeQuery.data.audio_url);
-  const transcription = await transcribeAudio({
-    language: episodeQuery.data.show?.language ?? undefined,
-    url,
-  });
-
-  const updateEpisodeContentQuery = await supabase
-    .from('episode_content')
-    .upsert(
-      {
-        account: await getAccountId(),
-        episode: id,
-        transcription,
-      },
-      {
-        onConflict: 'episode',
-      },
-    )
-    .select('transcription')
-    .single();
-
-  if (updateEpisodeContentQuery.error) {
-    throw new DatabaseError(updateEpisodeContentQuery.error);
-  }
-
-  return transcription;
 };
